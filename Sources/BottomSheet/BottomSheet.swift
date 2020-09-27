d8b608255325907f9c7e4b4e60c54767ed9d118b
Gaetano Matonti <gaetanomatonti@icloud.com>
2020-09-27 10:52:30 +0200

    Initial Commit

---
+++ Sources/BottomSheet/BottomSheet.swift
@@ -0,0 +1,3 @@
+struct BottomSheet {
+    var text = "Hello, World!"
+}
---
+++ Tests/BottomSheetTests/BottomSheetTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import BottomSheet
+
+final class BottomSheetTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BottomSheet().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
